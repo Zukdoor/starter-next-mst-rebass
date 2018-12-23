c323a49983c24149a1144a87272ddf6d6f1f78a2
Gabriel Wu <qqbbnease1004@126.com>
2018-12-23 11:34:35 +0800

    feat: add error page

--- pages/_error.js
+++ pages/_error.js
@@ -1,18 +1,18 @@
 import {Box, Card, Flex, Image, Heading, Text} from 'rebass'
 
-const photo = '/static/images/photo.jpeg'
+const notFound = '/static/images/404.png'
 
 export default () => (
   <div>
     <Box p={2} width={[1, 3 / 4, 1 / 2]} css={{margin: '0 auto'}}>
       <Card p={1} borderRadius={2} boxShadow="0 0 16px rgba(0, 0, 0, .25)">
-        <Image width={1} src={photo} />
+        <Image width={1} src={notFound} />
         <Box p={1}>
           <Heading as="h3" textAlign="center">
-            Zukdoor Template: Next.js
+            Sorry, we cannot find this page.
           </Heading>
           <Text fontSize={0} textAlign="center">
-            A template project based on Next.js
+            Feel free to contact us.
           </Text>
         </Box>
       </Card>
